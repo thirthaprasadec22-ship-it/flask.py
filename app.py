7384f085223dd640f96509b33ed6e27969a1ba1c
thirthaprasadec22-ship-it <thirthaprasad.ec22@sahyadri.edu.in>
2026-03-16 13:02:38 +0530

    flask

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "About Me"
+    return render_template("about.html")
 
 @app.route("/contact")
 def contact():
-    return "Contact Page"
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
